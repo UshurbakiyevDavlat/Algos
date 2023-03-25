9f3a26f3f7a1d9c249985f1be371dffa3c05a97d
Давлат Ушурбакиев <dushurbakiev@gmail.com>
2023-03-25 17:10:16 +0600

    updated with binary search solution

--- lostNumbers.ts
+++ lostNumbers.ts
@@ -1,25 +1,26 @@
+// Сложность O(LogN) Решение бинарным поиском
 function findMissingNumbers(len: number, arr: number[]) {
+    let missing = [];
+    let low = 0, high = arr.length - 1;
 
-    /*
-    1) По длине последовательности создаем массив и в каждой итерации сравниваем значения
-    и если нет совпадения, то добавляем в массив.
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        let leftCount = arr[mid] - arr[low] - (mid - low);
+        let rightCount = arr[high] - arr[mid] - (high - mid);
 
-    2) По оценку сложность алгоритма здесь, O(n) - линейная сложность, т.к. мы проходим по всем элементам хотя бы один раз.
-       Насколько я узнал, можно было бы сделать O(1), сделав через математические уравнения, но я что-то не догоняю как это сделать верно
+        if (leftCount > 0) {
+            missing.push(...Array.from({length: leftCount}, (_, i) => arr[low] + i));
+            low = mid + 1;
+        } else if (rightCount > 0) {
+            missing.push(...Array.from({length: rightCount}, (_, i) => arr[mid] + i));
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
 
-    P.S
-    В тайп скрипте нет функции includes по умолчанию поэтому пришлось arr.indexOf(num) === -1) смотреть по индексу.
-    В этой строчке (_, i) => i + 1) задаем ход последовательности.
-    А метод filter дает нам проверить каждое число последовательности на совпадение.
-
-    * */
-    const except = Array.from({ length: len }, (_, i) => i + 1).filter((num) => arr.indexOf(num) === -1);
-
-    except.forEach((i) => {
-        console.log(i);
-    });
-
+    const original = Array.from({length: len}, (_, i) => i + 1);
+    missing.forEach((i) => console.log(original[i]));
 }
 
-
-findMissingNumbers(10, [1, 2, 3, 4, 7, 8, 9, 10]);
+findMissingNumbers(10, [1, 4, 5, 6, 7, 8, 9, 10]);
